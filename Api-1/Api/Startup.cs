d76df815a576b8a9848ce5f4546ea989c23c03d5
Leandro Rauseo <leandro.rauseo@gmail.com>
2021-08-24 21:59:16 -0300

    ajustes finais do projeto

--- Api-1/Api/Startup.cs
+++ Api-1/Api/Startup.cs
@@ -45,12 +45,12 @@ namespace Api_1
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
+            //if (env.IsDevelopment())
+            //{
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Api_1 v1"));
-            }
+            //}
 
             app.UseHttpsRedirection();
 
--- Api-2/Api/Startup.cs
+++ Api-2/Api/Startup.cs
@@ -50,8 +50,8 @@ namespace Api
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
+            //if (env.IsDevelopment())
+            //{
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
                 app
@@ -59,7 +59,7 @@ namespace Api
                         c
                             .SwaggerEndpoint("/swagger/v1/swagger.json",
                             "Api v1"));
-            }
+            //}
 
             app.UseHttpsRedirection();
 
